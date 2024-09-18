695f33f862911d679f768f8d35e30291dc22b62b
Douglas Ferreira <silva.douglas37@gmail.com>
2024-09-18 14:17:47 -0300

    Removido arquivo de respostas

--- src/answers.py
+++
@@ -1,7 +0,0 @@
-df_grouped = df1[['id', 'seller_type']].groupby('seller_type')
-
-df_grouped = df_grouped.count().reset_index()
-
-df_grouped = df_grouped.rename(columns={'id': 'count'})
-
-df_grouped
