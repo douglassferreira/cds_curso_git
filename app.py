4ae2f7bdfd3683a1a9b8eba0341f7e117a6f15a2
Douglas Ferreira <109493867+douglassferreira@users.noreply.github.com>
2024-09-24 08:50:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,20 +15,13 @@ if __name__ == '__main__':
     main()
 
 
-# Função que irá criar 
-def create_sorted_companies_tuple(dataframe):
-    companies = dataframe['company_name'].to_list()
-    
-    companies_aux = [ company.split(' ') for company in companies ]
-    
-    companies_aux.sort(key=len, reverse=True)
-    
-    sorted_companies = [ ' '.join(company) for company in companies_aux ]
-    
-    return tuple(sorted_companies)
-
-
-
+def create_answers_section(df): 
+    st.title("Main Questions Answers")
+    st.header("First Round")
+    st.subheader("How many bikes are being sold by their owners and how many bikes are being sold by distributors?")
+    st.subheader("How many bikes are being sold are bikes from a unique owner?")
+    st.subheader("Are high kilometer bikes more expensive than bikes with lower kilometer?")
+    return None
 
 # Função que irá criar 
 def create_sorted_companies_tuple(dataframe):
